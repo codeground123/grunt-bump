8b531659c68517ffade4b4d6821661f6747c8046
Phani <raghavendra.rupakula@cdk.com>
2016-03-25 10:36:08 +0530

    test commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,8 +37,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-bump');
 
-
-    // Default task(s).
     grunt.registerTask('default', ['min']);
 
 };
